61deb90dd320030ed97cde635ddf9af9279cc05d
TomScott <contact@tascott.co.uk>
2023-02-17 17:52:00 +0000

    Start of function layout and procedure

--- index.js
+++ index.js
@@ -1,22 +1,51 @@
 const fs = require("fs");
-const path = require('path');
+// const path = require('path');
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
-console.log('Test')
+const licenseOptions = [
+    'Apache License 2.0',
+    'GNU General Public License v3.0',
+    'MIT License',
+    'BSD 2-Clause "Simplified" License',
+    'BSD 3-Clause "New" or "Revised" License',
+    'Boost Software License 1.0',
+    'Creative Commons Zero v1.0 Universal',
+    'Eclipse Public License 2.0',
+]
 
 // array of questions for user
 const questions = [
-
+    { type: 'input', name: 'title', message: 'What is the project title?' },
+    { type: 'input', name: 'description', message: 'What is the project description?' },
+    { type: 'input', name: 'installation', message: 'What are the installation instructions?' },
+    { type: 'input', name: 'usage', message: 'What is the usage information?' },
+    { type: 'input', name: 'contribution', message: 'What are the contribution guidelines?' },
+    { type: 'input', name: 'test', message: 'What are the test instructions?' },
+    { type: 'input', name: 'github', message: 'What is your GitHub username?' },
+    { type: 'input', name: 'email', message: 'What is your email address?' },
+    { type: 'list', name: 'license', message: 'What is the license?', choices: licenseOptions },
 ];
-
-// function to write README file
-function writeToFile(fileName, data) {
-}
 
 // function to initialize program
 function init() {
+    inquirer.prompt(questions)
+    .then((response) => {
+        //jsonify the response
+        console.log(response);
+        const stringToAppend = JSON.stringify(response);
+        console.log(stringToAppend);
+        //write to file
+        writeToFile('README.md', generateMarkdown(response));
+    });
+}
 
+// function to write README file
+function writeToFile(fileName, data) {
+    // fs.appendFile('log.txt', stringToAppend, (err) =>
+        //     err ? console.error(err) : console.log('Success!')
+        // );
+    console.log('writeToFile');
 }
 
 // function call to initialize program
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,5 +1,6 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
+  console.log('generateMarkdown');
   return `# ${data.title}
 
 `;
